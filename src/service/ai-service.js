1837c39560b2b62a11c05240e1a1d27bed659880
Mahender Sharma <sharma27july1999@gmail.com>
2026-05-17 13:44:13 +0530

    Update model version in generateResponse function to gemini-2.5-flash

--- src/service/ai-service.js
+++ src/service/ai-service.js
@@ -4,7 +4,7 @@ const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
 async function generateResponse(content) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     contents: content,
   });
   console.log(response.text);
